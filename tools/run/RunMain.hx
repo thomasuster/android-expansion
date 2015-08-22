9af9c8ddba647756379f6bf0d6421dbbaa3bb76a
Thomas Uster <thomasuster@gmail.com>
2015-08-21 17:33:08 -0700

    Added initial run script.

---
+++ tools/run/RunMain.hx
@@ -0,0 +1,11 @@
+import Array;
+class RunMain
+{
+    public static function main() {
+        log('haxe-android-expansion-util');
+    }
+
+    public static function log(s:String) {
+        Sys.println(s);
+    }
+}
