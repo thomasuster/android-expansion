c8c93fd019f6ef012c91748fa6c989125dfc8d28
Thomas Uster <thomasuster@gmail.com>
2015-08-22 12:26:07 -0700

    coverage on zip and push

--- tools/src/com/thomasuster/expansion/ExpansionCommandFactory.hx
+++ tools/src/com/thomasuster/expansion/ExpansionCommandFactory.hx
@@ -1,13 +1,23 @@
 package com.thomasuster.expansion;
 
-class ExpansionTool
+class ExpansionCommandFactory
 {
     public var args:Array<String>;
 
+    var command:Command;
+
     public function new():Void {}
 
-    public function makeZip():Command {
-        var command:Command = new Command();
+    public function make():Command {
+        command = new Command();
+        if(args[0] == 'zip')
+            makeZip();
+        if(args[0] == 'push')
+            makePush();
+        return command;
+    }
+
+    function makeZip():Void {
         command.name = args[0];
         var pack:String = getArg(0);
         var version:Int = Std.parseInt(getArg(1));
@@ -15,7 +25,16 @@ class ExpansionTool
         var dest:String = getLastArg();
         command.cwd = dest;
         command.args = ['-r', 'main.$version.$pack.obb', '$source'];
-        return command;
+    }
+
+    function makePush():Void {
+        command.name = 'adb';
+        var pack:String = getArg(0);
+        var version:Int = Std.parseInt(getArg(1));
+        var dest:String = getLastArg();
+        var androidDestination:String = '/mnt/shell/emulated/obb';
+        var name:String = 'main.$version.$pack.obb';
+        command.args = ['push', '$dest$name', '$androidDestination/$pack/$name'];
     }
 
     public function getArg(i:Int):String {
--- tools/test/TestTool.hx
+++ tools/test/TestTool.hx
@@ -1,28 +1,42 @@
 import com.thomasuster.expansion.Command;
-import com.thomasuster.expansion.ExpansionTool;
+import com.thomasuster.expansion.ExpansionCommandFactory;
 class TestTool extends haxe.unit.TestCase {
 
-    var tool:ExpansionTool;
+    var tool:ExpansionCommandFactory;
     var command:Command;
 
     override public function setup():Void {
-        tool = new ExpansionTool();
+        tool = new ExpansionCommandFactory();
     }
 
-    public function testZipCase() {
+    public function testZip() {
         tool.args = [];
         tool.args.push('zip');
         tool.args.push('com.companany.project');
         tool.args.push('178');
         tool.args.push('main-expansion');
         tool.args.push('/Users/person/project/');
-        command = tool.makeZip();
+        command = tool.make();
         assertTrue(command.name == 'zip');
         assertTrue(command.cwd == '/Users/person/project/');
         assertTrue(command.args.length == 3);
         assertTrue(command.args[0] == '-r');
         assertTrue(command.args[1] == 'main.178.com.companany.project.obb');
         assertTrue(command.args[2] == 'main-expansion');
+    }
+
+    public function testPush() {
+        tool.args = [];
+        tool.args.push('push');
+        tool.args.push('com.companany.project');
+        tool.args.push('178');
+        tool.args.push('/Users/person/project/');
+        command = tool.make();
+        assertTrue(command.name == 'adb');
+        assertTrue(command.args.length == 3);
+        assertTrue(command.args[0] == 'push');
+        assertTrue(command.args[1] == '/Users/person/project/main.178.com.companany.project.obb');
+        assertTrue(command.args[2] == '/mnt/shell/emulated/obb/com.companany.project/main.178.com.companany.project.obb');
     }
 
 }
