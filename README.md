f7157745913d38e3cfa0c6433a222e7f47a07ab1
Thomas Uster <thomasuster@gmail.com>
2015-08-22 11:21:38 -0700

    pushing to device working

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-haxelib run haxe-android-expansion zip main-expansion io.nme.samples.displayingabitmap 200 main-expansion
+haxelib run haxe-android-expansion zip io.nme.samples.displayingabitmap 200 main-expansion
+haxelib run haxe-android-expansion push io.nme.samples.displayingabitmap 200
--- tools/run/RunMain.hx
+++ tools/run/RunMain.hx
@@ -1,21 +1,23 @@
 import Array;
 class RunMain
 {
+
     public static function main() {
         log('haxe-android-expansion-util');
-        trace(Sys.args()[0]);
-        trace(Sys.args()[1]);
+        trace(Sys.args());
         var command:String = Sys.args()[0];
         log('command = $command');
         if(command == 'zip')
             zip();
+        if(command == 'push')
+            push();
     }
 
     static function zip() {
         var pack:String = getArg(0);
         var version:Int = Std.parseInt(getArg(1));
         var source:String = getArg(2);
-        var dest:String = getArg(3);
+        var dest:String = getLastArg();
         log('pack = $pack');
         log('version = $version');
         log('source = $source');
@@ -23,27 +25,36 @@ class RunMain
         log('dest = $dest');
 
         log(Sys.getCwd());
-        var requiredArgs:Array<String> = ['-r', '${dest}main.$pack.$version.obb', '$dest$source'];
+        var requiredArgs:Array<String> = ['-r', '${dest}main.$version.$pack.obb', '$dest$source'];
         trace(requiredArgs);
         Sys.command('zip', requiredArgs);
     }
 
+    static function push() {
+        var pack:String = getArg(0);
+        var version:Int = Std.parseInt(getArg(1));
+        var dest:String = getLastArg();
+        //adb push main.200.io.nme.samples.displayingabitmap.obb /mnt/shell/emulated/obb/io.nme.samples.displayingabitmap
+        var requiredArgs:Array<String> = ['push', '${dest}main.$version.$pack.obb', '/mnt/shell/emulated/obb/$pack'];
+        trace(requiredArgs);
+        trace(Sys.getCwd());
+        Sys.command('adb', requiredArgs);
+    }
+
     public static function getArg(i:Int):String {
-        if(Sys.args().length > i+2) {
-            return Sys.args()[i+2];
+        if(Sys.args().length > i) {
+            return Sys.args()[i+1];
         }
         return '';
+    }
+
+    public static function getLastArg():String {
+        return Sys.args()[Sys.args().length-1];
     }
 
     public static function log(s:String) {
         Sys.println(s);
     }
-//
-//    if(arg == 'unzip') {
-//var fileContent = File.getContent('../client/shared.nmml');
-//var versionParser:VersionParser = injector.instantiate(VersionParser);
-//var version:Int = versionParser.parse(fileContent);
-//var requiredArgs:Array<String> = ['../client/Export/main.proto.games.$version.obb', '-d', '../client/assets/expansion-main'];
-//Sys.command('unzip', requiredArgs);
-//}
+
+
 }
