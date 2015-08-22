c06f45dfc52bab430b6fb0e0699b144d327a52b0
Thomas Uster <thomasuster@gmail.com>
2015-08-22 13:55:50 -0700

    refactor

--- src/extension/androidExpansion/ExpansionReader.hx
+++ src/extension/androidExpansion/ExpansionReader.hx
@@ -7,63 +7,42 @@ import sys.FileSystem;
 import flash.display.BitmapData;
 class ExpansionReader {
 
-    public function new():Void {}
+    public var version:Int;
+
+    var reg:EReg;
+
+    public function new():Void {
+        reg = ~/[\/\\]/g;
+    }
 
     public function getBitmapData():BitmapData {
-        var version:Int = 178;
-        var localStoragePath : String = AndroidExpansion.getLocalStoragePath();
-        trace(localStoragePath);
-        var packageName : String = AndroidExpansion.getPackageName();
-        trace(packageName);
-        var assetsDirectory : String = localStoragePath + "/Android/obb/" + packageName + "/";
-        trace(assetsDirectory);
-        var assetsFilePath : String = assetsDirectory + "main." + version + "." + packageName + ".obb";
-
-
-        trace(assetsFilePath);
+        var localStoragePath:String = AndroidExpansion.getLocalStoragePath();
+        var packageName:String = AndroidExpansion.getPackageName();
+        var assetsDirectory:String = localStoragePath + "/Android/obb/" + packageName + "/";
+        var assetsFilePath:String = assetsDirectory + "main." + version + "." + packageName + ".obb";
 
         if(FileSystem.exists(assetsFilePath)) {
-            var assetsFile : FileInput = File.read( assetsFilePath );
-            var assetsEntries = Reader.readZip( assetsFile );
-
-            var ignoreRootFolder = "";
-            trace('Any entries?');
-            for(entry in assetsEntries) {
-
-                var fileName = entry.fileName;
+            var file:FileInput = File.read(assetsFilePath);
+            var entries = Reader.readZip(file);
+            for(entry in entries) {
+                var fileName:String = entry.fileName;
                 if (fileName.charAt (0) != "/" && fileName.charAt (0) != "\\" && fileName.split ("..").length <= 1) {
-                    var dirs = ~/[\/\\]/g.split(fileName);
-                    if ((ignoreRootFolder != "" && dirs.length > 1) || ignoreRootFolder == "") {
-                        if (ignoreRootFolder != "") {
-                            dirs.shift ();
-                        }
-
-                        var path = "";
-                        var file = dirs.pop();
-                        for( d in dirs ) {
-                            path += d;
-//                            sys.FileSystem.createDirectory(_dest + "/" + path);
-                            path += "/";
-                        }
-
-                        if( file == "" ) {
-                            if( path != "" ) trace("created " + path);
-                            continue; // was just a directory
-                        }
-                        path += file;
-                        trace("unzip " + path);
-                        if(path == 'main-expansion/nme.png') {
-                            var data = Reader.unzip(entry);
-                            var byteArray:ByteArray = ByteArray.fromBytes(data);
-                            return BitmapData.loadFromBytes(byteArray);
-                        }
+                    var dirs:Array<String> = reg.split(fileName);
+                    var path:String = '';
+                    var file = dirs.pop();
+                    for( d in dirs ) {
+                        path += d;
+                        path += "/";
+                    }
+                    path += file;
+                    if(path == 'main-expansion/nme.png') {
+                        var data = Reader.unzip(entry);
+                        var byteArray:ByteArray = ByteArray.fromBytes(data);
+                        return BitmapData.loadFromBytes(byteArray);
                     }
                 }
             }
-            trace('End of entries');
         }
-        else
-            trace('assetsFilePath ' + assetsFilePath + ' does not exist. $assetsFilePath');
         return null;
     }
 }
